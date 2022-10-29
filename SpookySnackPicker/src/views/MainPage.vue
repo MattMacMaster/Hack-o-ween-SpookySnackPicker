7862b9c6f3c9fa7c0f034edbb6e5ad5b793cb685
Matthew W <fatboychummy@gmail.com>
2022-10-29 13:18:17 -0600

    test out this fancy freakin table

--- SpookySnackPicker/src/views/MainPage.vue
+++ SpookySnackPicker/src/views/MainPage.vue
@@ -1,7 +1,12 @@
+<script setup>
+  import CandySetVue from '../components/Bowl/CandySet.vue';
+</script>
+
 <template>
     <div id="app">
       <h1>BIG Main Page</h1>
          
+      <CandySetVue />
     </div>
 
 </template>
