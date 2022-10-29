43d42a446f1c285bd3fde75d48256fcf3740c6f2
Matthew W <fatboychummy@gmail.com>
2022-10-29 01:01:26 -0600

    This is a mess currently but it works kinda

--- SpookySnackPicker/src/components/Bowl/CandySet.vue
+++ SpookySnackPicker/src/components/Bowl/CandySet.vue
@@ -1,16 +1,426 @@
-<!-- CandySets hold Candies -->
+<!-- CandySets hold Candies (grid of items) -->
 
 <script setup>
 
 </script>
 
+<!--competitorname,chocolate,fruity,caramel,peanutyalmondy,nougat,crispedricewafer,hard,bar,pluribus,sugarpercent,pricepercent,winpercent-->
 
 <template>
-  
+  <table class="candy-container">
+    <thead>
+      <tr>
+        <th>Candy Name</th>
+        <th>Chocolatey?</th>
+        <th>Fruity?</th>
+        <th>Caramely?</th>
+        <th>Peanuty or Almondy?</th>
+        <th>Nougaty?</th>
+        <th>CrispRicey?</th>
+        <th>Hard?</th>
+        <th>Bar?</th>
+        <th>Pluribus?</th>
+        <th>Sugar</th>
+        <th>Price</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody class="scrollable">
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+      <tr>
+        <th>M&Ms</th>
+        <th>Yep</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Nope</th>
+        <th>Yep</th>
+        <th>Enough</th>
+        <th>A bit</th>
+        <th>Pretty good</th>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 
 
 <style scoped>
+  .candy-container {
+    border: 3px solid purple;
+    height: 100%;
+    width: 100%;
+    background-color: darkcyan;
+    margin: auto;
+    border-collapse: collapse;
+    color: white;
+  }
 
+  .candy-container thead tr th {
+    font-weight: bold;
+    padding: 3px;
+    border-bottom: 5px solid purple;
+    border-left: 2px dotted purple;
+    border-right: 2px dotted purple;
+    background-color: green;
+  }
+
+  .candy-container tbody tr th {
+    padding: 3px;
+    border-left: 2px dotted purple;
+    border-right: 2px dotted purple;
+    border-top: 2px dotted purple;
+  }
+
+  .candy-container tbody tr:nth-child(odd) {
+    background-color: rgba(0, 255, 255, 0.733);
+  }
+
+  .candy-container * tr th:first-child {
+    border-right: 2px dotted purple;
+    border-left: 0;
+  }
+
+  .candy-container * tr th:last-child {
+    border-left: 2px dotted purple;
+    border-right: 0;
+  }
+
+  table, tr td {
+    border: 1px solid red
+  }
+  tbody {
+      display: block;
+      height: 100px;
+      overflow: auto;
+      overflow-y: scroll;
+  }
+  thead, tbody tr {
+      display: table;
+      width: 100%;
+      table-layout: fixed;/* even columns width , fix width of table too*/
+  }
+  thead {
+      width: calc( 100% - 1em )/* scrollbar is average 1em/16px width, remove it from thead width */
+  }
+  table {
+      width: 400px;
+  }
 </style>
