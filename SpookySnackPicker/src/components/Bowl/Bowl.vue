909287700e111f14d75d46769c0c6407749721d5
Matthew W <fatboychummy@gmail.com>
2022-10-29 14:51:48 -0600

    Add basic bowl stuff idk what im doing tho but it looks to work somewhat maybe idk who knows but i definitely dont lmao please help

--- SpookySnackPicker/src/components/Bowl/Bowl.vue
+++ SpookySnackPicker/src/components/Bowl/Bowl.vue
@@ -1,16 +1,66 @@
-<!-- Bowls hold candy sets -->
-
-<script setup>
-
-</script>
-
-
 <template>
-  
+  <EasyDataTable
+    :headers="headers"
+    :items="items"
+  />
 </template>
 
+<script lang="ts">
+import type { Header, Item } from "vue3-easy-data-table";
 
+import { defineComponent } from 'vue';
 
-<style scoped>
+export default defineComponent({
+  setup() {
+    const headers: Header[] = [
+      { text: "Component", value: "component" },
+      { text: "Items", value: "items" },
+      { text: "Edit", value: "edit" }
+    ];
 
+    const items: Item[] = [
+      { "component": "Chocolate", "items": null, "edit": "EDIT" },
+      { "component": "Fruit", "items": null, "edit": "EDIT" },
+      { "component": "Caramel", "items": null, "edit": "EDIT" },
+      { "component": "Peanut or Almond", "items": null, "edit": "EDIT" },
+      { "component": "Nougat", "items": null, "edit": "EDIT" },
+      { "component": "Crisped Rice", "items": null, "edit": "EDIT" },
+      { "component": "Hard", "items": null, "edit": "EDIT" },
+      { "component": "Bar", "items": null, "edit": "EDIT" },
+      { "component": "Pluribus", "items": null, "edit": "EDIT" }
+    ];
+
+    return {
+      headers,
+      items
+    };
+  },
+});
+</script>
+
+<style>
+  .container{
+    z-index: 0;
+    box-shadow: 8px 8px rgb(0 0 0 / 0.3);
+    border-style: solid;
+    border-color: black;
+    background-color: gray;
+  }
+
+  thead tr th:first-of-type {
+    width: 15em;
+  }
+
+  thead tr th:last-of-type {
+    width: 5em;
+  }
+  
+  div.vue3-easy-data-table div.vue3-easy-data-table__footer {
+    display: none;
+  }
+
+  td:nth-of-type(3) {
+    color: blue;
+    text-decoration-line: underline;
+  }
 </style>
